714005785cace7b3779bda60f27031400c3ebaf3
Kyle Mar <kwm18123@gmail.com>
2023-03-03 01:10:38 -0500

    Plot Audio Signal

--- MicStreaming.py
+++ MicStreaming.py
@@ -1,22 +1,18 @@
 import pyaudio
-import wave
 import sys
-import queue
+import time
+import wave
 import matplotlib.pyplot as plt
-from matplotlib.animation import FuncAnimation
 import numpy as np
 
-fs = 44100 # samples per second
-FORMAT = pyaudio.paInt16 # 16 bits per sample
-CHANNELS = 1 # Audio Channels
-chunk = 1024 # record in chunks of 1024 samples
+fs = 44100  # samples per second
+FORMAT = pyaudio.paInt16  # 16 bits per sample
+CHANNELS = 1  # Audio Channels
+chunk = 1024  # record in chunks of 1024 samples
+frames = []  # stores recorded data
+save_length = 7.5  # Length of audio data saved from end
+start_time = time.time()
 filename = "output.wav"
-
-#def callback(in_data, frame_count, time_info, status):
- #   data = np.frombuffer(in_data, dtype=np.Int16)
-  #  print(data)
-
-
 
 audio = pyaudio.PyAudio()
 stream = audio.open(format=FORMAT,
@@ -25,13 +21,20 @@ stream = audio.open(format=FORMAT,
                     input=True,
                     frames_per_buffer=chunk)
 
-frames = [] # stores recorded data
-# Store data in chunks for 3 seconds
-for i in range(0, int(fs/ chunk * 10)):
-    data = stream.read(chunk)
-    frames.append(data)
 
-# Stop and close the stream
+while time.time() - start_time < 10:
+    # Get Audio Data into frame. runs for time interval
+    data = np.frombuffer(stream.read(chunk), dtype=np.int16)
+    frames.append(data)
+    # Only keep the last 300 chunks (7.5 seconds)
+    frames = frames[-int(((fs / chunk) * save_length)):]
+    # plot frames
+
+amplitude = np.hstack(frames)
+Time = np.linspace(0, len(amplitude) / fs, num=len(amplitude))
+plt.plot(Time, amplitude)
+plt.show()
+
 stream.stop_stream()
 stream.close()
 audio.terminate()
@@ -42,15 +45,3 @@ wf.setsampwidth(audio.get_sample_size(FORMAT))
 wf.setframerate(fs)
 wf.writeframes(b''.join(frames))
 wf.close()
-
-spf = wave.open("output.wav", "r")
-signal = spf.readframes(-1)
-signal = np.frombuffer(signal, dtype=np.int16)
-fs = spf.getframerate()
-Time = np.linspace(0, len(signal) / fs, num=len(signal))
-
-plt.figure(1)
-plt.title("Signal Wave...")
-plt.plot(Time, signal)
-plt.show()
-# Start animation
