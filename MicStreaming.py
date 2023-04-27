fe7f62e65bb97dd94aeba22dffe7a49864fc75a4
Kyle Mar <kwm18123@gmail.com>
2023-04-27 16:29:50 -0400

    Added Buzzer Source Code

--- MicStreaming.py
+++ MicStreaming.py
@@ -4,7 +4,6 @@ import time
 import matplotlib.pyplot as plt
 import numpy as np
 
-
 import filter
 import sounddevice as sd
 from scipy.fft import fft, ifft, fftfreq
@@ -18,7 +17,7 @@ class Streaming(object):
         self.FORMAT = pyaudio.paInt16  # 16 bits per sample
         self.CHANNELS = 1  # Audio Channels
         self.chunk = 1024  # record in chunks of 1024 samples
-        self.device_index = None # Specify input device
+        self.device_index = None  # Specify input device
         self.save_length = .25  # Length of audio data saved from end (s)
         self.t = []  # stores time corresponding to each sample
         self.frames = []  # stores recorded data
@@ -28,7 +27,6 @@ class Streaming(object):
         self.tv = []
         self.stream_open = False
         self.audio_data = []
-
 
     def start_recording(self):
         self.stream_open = True
@@ -47,10 +45,9 @@ class Streaming(object):
                 data = np.frombuffer(self.stream.read(self.chunk), dtype=np.int16)
                 self.t.append(self.stream.get_time())
                 self.frames.append(data)
-                # Only keep the last 300 chunks (5 seconds)
+                # Only keep the last portion of data (specified in save_length)
                 self.frames = self.frames[-int(((self.fs / self.chunk) * self.save_length)):]
                 self.t = self.t[-len(self.frames):]
-
                 self.stream_open = True
         except:
             print("Stream Stopped: Exception")
@@ -67,7 +64,7 @@ class Streaming(object):
         a2 = np.hstack(mic2.frames)
         self.create_space()
         mic2.create_space()
-        figure, axis = plt.subplots(1,2)
+        figure, axis = plt.subplots(1, 2)
         axis[0].plot(self.tv, a1)
         axis[1].plot(mic2.tv, a2)
         plt.show()
@@ -87,7 +84,6 @@ class Streaming(object):
         if time.time() - self.start_time > 1:
             t_data = self.t
             raw_data = self.frames
-            # Create a linspace of the data with time values for each sample
             first = t_data[0]
             last = t_data[-1]
             self.audio_data = np.hstack(raw_data)
@@ -99,7 +95,7 @@ class Streaming(object):
         time1_origin = self.tv[0]
         time2_origin = mic2.tv[0]
         diff = time2_origin - time1_origin
-        index_shift = abs(int(diff/ (1/self.fs)))
+        index_shift = abs(int(diff / (1 / self.fs)))
         if diff > 0:
             # Mic2 starts after mic1 by index_shift samples
             self.tv = self.tv[index_shift:]
@@ -113,42 +109,42 @@ class Streaming(object):
             mic2.tv = mic2.tv[index_shift:]
             mic2.audio_data = mic2.audio_data[index_shift:]
 
-
-
-    def filter(self, filter_freq, plot=False):
+    def filter(self, filter_freq, plot=False, normalize=True):
         lowBound = (filter_freq - 100)
         highBound = (filter_freq + 100)
+        if normalize:
+            self.audio_data = filter.normalize(self.audio_data)
 
-        self.audio_data = filter.normalize(self.audio_data)
         transform = fft(self.audio_data)
-
-        # sos = signal.butter(4, [lowBound, highBound], btype='bandpass')
         N = len(transform)
         xf = fftfreq(transform.size, 1 / self.fs)
         fTransform = np.copy(transform)
         fTransform[np.abs(xf) <= lowBound] = 0
         fTransform[np.abs(xf) >= highBound] = 0
-
         filteredAmplitude = ifft(fTransform)
-        filteredAmplitude = filter.normalize(filteredAmplitude)
+
+        if normalize:
+            filteredAmplitude = filter.normalize(filteredAmplitude)
         fn = len(filteredAmplitude)
-        # amplitude = np.hstack(self.frames)
         amplitude = self.audio_data
-        # Plot Raw audio and FFT of both filtered and unfiltered amplitude
+
         if plot:
+            # Plot Raw audio and FFT of both filtered and unfiltered amplitude
             xt = np.linspace(0, len(amplitude) / self.fs, num=len(amplitude))
             fig, ax = plt.subplots(1, 4)
             ax[0].plot(xf[:(N - 1)], transform[:(N - 1)], 'r')
-            ax[1].plot(xt, amplitude,  'b')
+            ax[1].plot(xt, amplitude, 'b')
             ax[2].plot(xf[:(N - 1)], fTransform[:(N - 1)], 'r')
             ax[3].plot(xt, filteredAmplitude, 'g')
             plt.show()
 
         return filteredAmplitude
 
-
     def cross_correlation(self, mic2, freq):
         self.sync_time(mic2)
+        m1 = self.audio_data
+        m2 = mic2.audio_data
+
         fa1 = self.filter(freq, plot=True)
         fa2 = mic2.filter(freq, plot=True)
 
@@ -174,36 +170,67 @@ class Streaming(object):
         corr_normalized = corr / (C_norm1 * C_norm2)
         max_indices_back = -int(((1 / 10) / 2) / (1 / mic1.fs)) + center_index
         max_indices_forward = int(((1 / 10) / 2) / (1 / mic1.fs)) + center_index
-        i_max_C_normalized = np.argmax(corr_normalized)
+        i_max_C = np.argmax(corr_normalized[max_indices_back:max_indices_forward + 1])
+
         i_max_C_normalized = np.argmax(corr_normalized[max_indices_back:max_indices_forward + 1]) + max_indices_back
         t_shift_hat_normalized = t_shift_C[i_max_C_normalized]
         ind = len(corr_normalized)
+        #
+        # fig, ax = plt.subplots(1, 2)
+        # ax[0].plot(t_shift_C[:ind], corr_normalized)
+        # ax[1].plot(t_shift_C[max_indices_back:max_indices_forward], corr_normalized[max_indices_back:max_indices_forward])
+        # plt.show()
 
-        plt.plot(t_shift_C[:ind], corr_normalized)
-        plt.show()
+        m1_ind = np.argmax(np.sum(m1[max_indices_back:max_indices_forward]))
+        m2_ind = np.argmax(np.sum(m2[max_indices_back:max_indices_forward]))
+        print('Mag1:', m1[m1_ind])
+        print('Mag2:', m2[m2_ind])
 
+        center_ind = t_shift_C
         return t_shift_hat_normalized
-
 
     def time_delay(self, mic2, freq):
         while self.stream_open and mic2.stream_open:
             delay = mic1.cross_correlation(mic2, freq)
             tdoa = abs(delay)
-            print('Time Delay: ', tdoa)
-            print('Distance (m):', tdoa * 343)
+            # print('Time Delay: ', tdoa)
+            # print('Distance (m):', tdoa * 343)
             time.sleep(.25)
+
+    def magnitude(self):
+        while self.stream_open:
+            self.create_space()
+
+            filteredAmp = mic1.filter(4000, plot=False, normalize=False)
+            fmax_ind = np.argmax(filteredAmp)
+            fmax = abs(filteredAmp[fmax_ind])
+
+            # Average out the top n indices to get a more stable magnitude
+            n = 50
+            descend = filteredAmp.argsort()[::-1]
+            sorted_fAmp = filteredAmp[descend]
+            top_n_mag = sorted_fAmp[0:n]
+            fmax_avg = abs(np.mean(top_n_mag))
+
+            # print('Filtered Magnitude: ', fmax)
+            print('Average Filtered Magnitude: ', fmax_avg)
 
 
 if __name__ == "__main__":
     mic1 = Streaming()
     mic1.device_index = 1
     mic1.start_recording()
-    mic2 = Streaming()
-    mic2.device_index = 2
-    mic2.start_recording()
+    # mic2 = Streaming()
+    # mic2.device_index = 2
+    # mic2.start_recording()
     thread1 = threading.Thread(target=mic1.stream_read)
-    thread2 = threading.Thread(target=mic2.stream_read)
+    # thread2 = threading.Thread(target=mic2.stream_read)
     thread1.start()
-    thread2.start()
+    # thread2.start()
     time.sleep(2)
-    D = mic1.time_delay(mic2, 4000)
+    # D = mic1.time_delay(mic2, 4000)
+    # delay = mic1.cross_correlation(mic2, 4000)
+    # tdoa = abs(delay)
+    # print('Time Delay: ', tdoa)
+    # print('Distance (m):', tdoa * 343)
+    mic1.magnitude()
