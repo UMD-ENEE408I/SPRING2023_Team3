ad4c546fa8145975a06dfa99551f9f9dd1eed507
Kyle Mar <kwm18123@gmail.com>
2023-05-01 10:43:25 -0400

    Updated MicStreaming to calculate the magnitude

--- main.py
+++ main.py
@@ -1,1 +1,14 @@
 import MicStreaming
+import numpy as np
+import time
+import threading
+
+# Initialize Microphone
+mic1 = MicStreaming.Streaming()
+mic1.device_index = 1
+mic1.start_recording()
+
+thread1 = threading.Thread(target=mic1.stream_read)
+thread1.start()
+time.sleep(2)
+mic1.magnitude()
